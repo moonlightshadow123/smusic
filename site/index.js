0ae5f2be9e38da64acdb94a5e267fffba11f146f
platformrd <2252822920@qq.com>
2020-08-25 21:22:46 -0400

    add simplebar

--- site/index.js
+++ site/index.js
@@ -76,7 +76,7 @@ function randomClick(){
 	var len = curData.length;
 	var idx = Math.floor(Math.random() * len);
 	var item = curData[idx]
-	window.open(item.url, '_blank');
+	openAbc(item.url);
 }
 
 function searchClick(){
